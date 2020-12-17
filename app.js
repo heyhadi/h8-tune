0befe9c88cfc5f334b1e6b77c6b6386c983c61bc
heyhadi <hadi.munawirul@gmail.com>
2020-12-18 02:11:24 +0700

    done

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const port = 3002
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
-app.use(session({secret: "Shh, its a secret!"}));
-
+app.use(session({
+  secret: 'pair project',
+  resave: false,
+  saveUninitialized: true,
+}))
 app.use('/', router)
 
 
