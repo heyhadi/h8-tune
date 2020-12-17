b8ebf339f89ff8556dd3e459aecd2431aa073757
heyhadi <hadi.munawirul@gmail.com>
2020-12-17 22:36:18 +0700

    crud done

---
+++ models/song.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Song extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Song.belongsToMany(models.User, {through:models.Playlist})
+    }
+  };
+  Song.init({
+    title: DataTypes.STRING,
+    artist: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    released_date: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Song',
+  });
+  return Song;
+};
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const Controller = require('../controllers/controller')
+
+function authenticate(req, res, next) {
+  
+    if (req.session.username) {
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
+
+router.get('/login', Controller.login)
+router.post('/login', Controller.loginPost)
+router.get('/register', Controller.register)
+router.post('/register', Controller.saveRegister)
+router.use(authenticate)
+router.get('/', Controller.home)
+
+
+// //Song
+router.get('/songs', Controller.songList)
+router.get('/songs/add', Controller.addGet)
+router.post('/songs/add', Controller.addSong)
+router.get('/songs/:id/delete', Controller.destroy)
+router.get('/songs/edit/:id', Controller.editForm)
+router.post('/songs/edit/:id', Controller.editSong)
+// router.get('/song/delete/:id', Controller.destroy)
+
+// router.get('/users', Controller.userList)
+// router.get('/users/add', Controller.addNewUser)
+// router.post('/users/add', Controller.saveNewUser)
+
+// router.get('/users/delete/:id', Controller.destroyUser)
+
+// router.get('/tutorials/favorite/:id', Controller.showFavorite)
+
+// router.get('/like/:id', Controller.like)
+
+
+
+
+module.exports = router
